d3f3913ba145d1fb9fc69fa0bd05988a72531d18
Madison-Cox <madison.cox5285@hotmail.com>
2022-10-19 16:24:11 -0500

    corrected error

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       <div className="App">
         <CitySearch />
         <EventList />
+        <NumberOfEvents />
       </div>
     );
   }
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,18 +3,13 @@ import React, { Component } from 'react';
 class NumberOfEvents extends Component {
   state = {
     NumberOfEvents: 32,
-    textWarning: '',
   };
 
   handleInputChange = (event) => {
     const value = event.target.value;
-    const warning = value < 1 ? 'Please enter a number greater than 0' : '';
     this.setState({
       NumberOfEvents: value,
-      textWarning: warning,
     });
-
-    this.props.updateEvents(undefined, value);
   };
 
   render() {
@@ -26,13 +21,11 @@ class NumberOfEvents extends Component {
             <input
               type='number'
               className='number-of-events-input'
-              min='1'
               value={this.state.NumberOfEvents}
               onChange={this.handleInputChange}
             />
           </label>
         </div>
-        <p>{this.state.textWarning}</p>
       </div>
     );
   }
