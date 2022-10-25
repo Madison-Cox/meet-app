28a26dd227208882425c31c0cc5716981867936b
Madison-Cox <madison.cox5285@hotmail.com>
2022-10-25 16:06:38 -0500

    edit number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
-import { getEvents } from './api';
+
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
   };
 
-  updateEvents = (location, eventCount) => {
-    const { numberOfEvents } = this.state;
-    if (location === undefined) location = this.state.selectedLocation;
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      eventCount = eventCount === undefined ? numberOfEvents : eventCount;
-      this.setState({
-        events: locationEvents.slice(0, eventCount),
-        selectedLocation: location,
-        numberOfEvents: eventCount,
-      });
-    });
-  }
 
   handleInputChange = (event) => {
     const value = event.target.value;
     this.setState({
       numberOfEvents: value,
     });
-    this.props.updateEvents(value);
+    this.state.updateEvents(value);
   };
-
-
 
 
   render() {
