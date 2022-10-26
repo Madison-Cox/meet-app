ecf979615b9a57e7fb8bf3f81be4825e51b60b88
Madison-Cox <madison.cox5285@hotmail.com>
2022-10-25 19:16:25 -0500

    edit app.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents numberOfEvents={numberOfEvents} updateEvents={this.updateEvents} />
+        <NumberOfEvents events={this.state.events} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
       </div>
     );
