c3346ac73d423c744d27435f0857ae1f239ef381
Madison-Cox <madison.cox5285@hotmail.com>
2022-11-01 12:26:02 -0500

    install atatus

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
+atatus.config('21ce3f55c1624d7bbeb633c873b90a37').install();
+atatus.notify(new Error('Test Atatus Setup'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
