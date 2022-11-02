d44571a3a3f9b84e6bdcc2702fe25b1977c98c9a
Madison-Cox <madison.cox5285@hotmail.com>
2022-11-02 13:25:19 -0500

    added alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,7 +1,8 @@
 // src/CitySearch.js
 
+import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 import React, { Component } from 'react';
-
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -21,19 +22,30 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions: [],
+        showSuggestions: false,
+        infoText: ''
+      });
+    }
   };
 
   render() {
 
     return (
       <div className='CitySearch'>
+        <InfoAlert text={this.state.infoText} />
         <input
           type='text'
           className='city'
